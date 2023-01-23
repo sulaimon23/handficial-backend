961a6da5a248b2e546e0a3c87e236c13c995c220
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-01-23 13:40:05 +0100

    doc: vercel deployment setup

--- routes/demo.route.js
+++ routes/demo.route.js
@@ -1,8 +1,10 @@
-const { Router } = require('express');
-const { SuccessResponseObject } = require('../common/http');
+const { Router } = require("express");
+const { SuccessResponseObject } = require("../common/http");
 
 const r = Router();
-
-r.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
+//
+r.get("/", (req, res) =>
+    res.json(new SuccessResponseObject("demo path live 🚀"))
+);
 
 module.exports = r;
