59b28c8d7eac654ad3e222850fcbe7fd61a1c4b4
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-01-24 15:09:53 +0100

    fixed

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ app.use(function (req, res, next) {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(helmet());
-// app.use("/", routes);
+app.use("/", routes);
 
 // default catch all handler
-app.all("/", (req, res) =>
+app.all("*", (req, res) =>
     res.status(404).json(new ErrorResponseObject("route not defined"))
 );
 
